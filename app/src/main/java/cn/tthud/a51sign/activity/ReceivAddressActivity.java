c0c1660dd77ae017aeadbd366846c77d19b5b31e
wblt <940422068@qq.com>
2018-03-27 16:26:57 +0800

    d

---
+++ app/src/main/java/cn/tthud/a51sign/activity/ReceivAddressActivity.java
@@ -0,0 +1,15 @@
+package cn.tthud.a51sign.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.tthud.a51sign.R;
+
+public class ReceivAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiv_address);
+    }
+}
